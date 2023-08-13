0f294af2e850489bc9d06b82551c76834c569687
abdo-mhmd <abdo202224@gmail.com>
2023-08-13 23:42:27 +0300

    update create table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
--- Prints the full description of the table `first_table`
--- from the database `hbtn_0c_0` in MySQL Server.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
+-- Print full descreption from table
 SHOW CREATE TABLE first_table;
