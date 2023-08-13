de57c206f6cb094b8b834ce8e9a67e266403f773
abdo-mhmd <abdo202224@gmail.com>
2023-08-13 23:33:05 +0300

    update create table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,10 +2,10 @@
 -- from the database `hbtn_0c_0` in MySQL Server.
 CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
+    name VARCHAR(128) charset latin1,
     c CHAR(1),
     created_at DATE,
     PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 SHOW CREATE TABLE first_table;
