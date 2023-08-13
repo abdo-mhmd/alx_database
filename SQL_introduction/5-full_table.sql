3723a7a7ba62b33a0932b5260986af3895765fe6
abdo-mhmd <abdo202224@gmail.com>
2023-08-13 23:41:20 +0300

    update create table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,10 @@
 -- Prints the full description of the table `first_table`
 -- from the database `hbtn_0c_0` in MySQL Server.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
 SHOW CREATE TABLE first_table;
