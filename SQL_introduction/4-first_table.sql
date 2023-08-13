1e04b42ad91d6f3d0dac9b39d4d4c83618d017a0
abdo-mhmd <abdo202224@gmail.com>
2023-08-13 21:34:57 +0300

    update create table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
 -- creates a table called first_table in the current database
+DROP DATABASE IF EXISTS hbtn_test_db_4;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
